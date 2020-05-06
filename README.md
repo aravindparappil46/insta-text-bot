15f8336d763862f890c6568635ee73768062f3df
aparappi <aparappi@iu.edu>
2020-05-06 02:17:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Instagram Direct Message Bot
+
+Automatically sends direct messages to a Instagram user whom you follow!
+
+Each message is sent one word at a time!
+
+
+## Requirements
+
+- Must have ```python3``` installed
+
+- Must have selenium installed. Do `pip install selenium`
+
+- ```ChromeDriver.exe``` based on the version of Google Chrome you are running. Go [here](https://chromedriver.chromium.org/downloads) to download
+
+- A file containing the message you want to send (can be in any format)
+
+- Must be able to login to Instagram via Facebook
+
+## How-to
+
+- Fill up the details in ```settings.ini```
+
+- Open up a terminal and run ``` python3 bot.py```  (or ```python bot.py``` depending on your environment setup. The code is written in Python 3)
+
+- Watch it work!
---
+++ bot.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as expect
+import configparser
+
+# ---------- Settings --------------- #
+
+config = configparser.ConfigParser()
+config.read('settings.ini')
+driver_path = config['Settings']['PathToChromeDriver']
+input_file = config['Settings']['Filename']
+username = config['Settings']['FBUsername']
+password = config['Settings']['FBPassword']
+user_id = config['Settings']['InstagramUserID']
+
+# ---------- End Of Settings --------------- #
+
+driver = webdriver.Chrome(driver_path)
+wait = WebDriverWait(driver, 10)
+
+driver.get("https://www.instagram.com/direct/inbox/")
+
+# Login with Facebook
+wait.until(expect.visibility_of_element_located((By.CSS_SELECTOR , ".KPnG0"))).click()
+driver.find_element_by_name("email").send_keys(username)
+driver.find_element_by_name("pass").send_keys(password)
+driver.find_element_by_name("pass").send_keys(Keys.RETURN)
+
+# Click on Not Now for notification pop-up
+wait.until(expect.visibility_of_element_located((By.CSS_SELECTOR , ".aOOlW.HoLwm"))).click()
+
+# Click on the user
+xpath = "//*[contains(text(),'"+user_id+"')]"
+wait.until(expect.visibility_of_element_located((By.XPATH , xpath))).click()
+
+# Send messages word-by-word
+with open(input_file,'r') as file:
+	msg_input = driver.find_element_by_xpath("//textarea[@placeholder='Message...']")
+	for line in file:
+		for word in line.split():
+			msg_input.send_keys(word)
+			msg_input.send_keys(Keys.RETURN)
